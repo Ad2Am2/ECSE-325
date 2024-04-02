f3deebcd39bfede303b2273852f612029e88abc9
Adam Corbier <adam.corbier@mail.mcgill.ca>
2024-04-02 15:15:16 -0400

    fix stupid copy paste thing ugh

--- g31_Hash_Core.vhd
+++ g31_Hash_Core.vhd
@@ -23,20 +23,37 @@ end g31_Hash_Core;
 
 architecture Behavioral of g31_Hash_Core is
 
-    component g31_SIG_CH_MAJ is 
-        port ( 
-            A_o, B_o, C_o, E_o, F_o, G_o  : in std_logic_vector(31 downto 0);        
-            SIG0, SIG1, CH, MAJ           : out std_logic_vector(31 downto 0)
-        );
-    end component;
+	component g31_SIG_CH_MAJ is 
+	  port ( 
+			A_o, B_o, C_o, E_o, F_o, G_o  : in std_logic_vector(31 downto 0);        
+			SIG0, SIG1, CH, MAJ           : out std_logic_vector(31 downto 0)
+	  );
+	end component;
 
-    -- Internal signals
-    signal A_reg, B_reg, C_reg, D_reg, E_reg, F_reg, G_reg, H_reg : std_logic_vector(31 downto 0);
-    signal Kt, Wt : std_logic_vector(31 downto 0);
-	 signal SIG0, SIG1, CH, MAJ : std_logic_vector(31 downto 0);
-	 signal A_reg_next, E_reg_next, RESTOFSTUFF : std_logic_vector(31 downto 0);
+	-- Internal signals
+	signal A_reg, B_reg, C_reg, D_reg, E_reg, F_reg, G_reg, H_reg : std_logic_vector(31 downto 0);
+	--    signal Kt, Wt : std_logic_vector(31 downto 0);
+	signal Wt : std_logic_vector(31 downto 0);
+	signal SIG0, SIG1, CH, MAJ : std_logic_vector(31 downto 0);
+	signal A_reg_next, E_reg_next, RESTOFSTUFF : std_logic_vector(31 downto 0);
+
+	type constant_array is array(0 to 63) of std_logic_vector(31 downto 0);
+	constant Kt : constant_array := ( x"428a2f98", x"71374491", x"b5c0fbcf", x"e9b5dba5",
+	x"3956c25b", x"59f111f1", x"923f82a4", x"ab1c5ed5", x"d807aa98", x"12835b01",
+	x"243185be", x"550c7dc3", x"72be5d74", x"80deb1fe", x"9bdc06a7", x"c19bf174",
+	x"e49b69c1", x"efbe4786", x"0fc19dc6", x"240ca1cc", x"2de92c6f", x"4a7484aa",
+	x"5cb0a9dc", x"76f988da", x"983e5152", x"a831c66d", x"b00327c8", x"bf597fc7",
+	x"c6e00bf3", x"d5a79147", x"06ca6351", x"14292967", x"27b70a85", x"2e1b2138",
+	x"4d2c6dfc", x"53380d13", x"650a7354", x"766a0abb", x"81c2c92e", x"92722c85",
+	x"a2bfe8a1", x"a81a664b", x"c24b8b70", x"c76c51a3", x"d192e819", x"d6990624",
+	x"f40e3585", x"106aa070", x"19a4c116", x"1e376c08", x"2748774c", x"34b0bcb5",
+	x"391c0cb3", x"4ed8aa4a", x"5b9cca4f", x"682e6ff3", x"748f82ee", x"78a5636f",
+	x"84c87814", x"8cc70208", x"90befffa", x"a4506ceb", x"bef9a3f7", x"c67178f2"
+	);
 
 begin
+
+	
 
     process (CLK)
     begin
