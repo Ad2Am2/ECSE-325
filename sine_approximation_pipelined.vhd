1c77a1c20ef4a3b4b777dcee455e124a07aeceb2
Ad2Am2 <92311639+Ad2Am2@users.noreply.github.com>
2024-04-05 12:57:54 -0400

    Fix really stupid error

--- sine_approximation_pipelined.vhd
+++ sine_approximation_pipelined.vhd
@@ -56,29 +56,29 @@ begin
     end process;
 
     angle_change <= "0000000000000000"&angle_32;
-    tmp1_1 <= std_logic_vector(unsigned(C1)*unsigned(angle_change));
+    tmp1_1 <= unsigned(unsigned(C1)*unsigned(angle_change));
     result1 <= "0000000000000"&tmp1_1(31 downto 13);
     
     --angle_reg_1 <= angle_change;
-    tmp1_2 <= std_logic_vector(unsigned(z_reg_1)*unsigned(angle_reg_1));
+    tmp1_2 <= unsigned(unsigned(z_reg_1)*unsigned(angle_reg_1));
     --tmp2 <= "000"&tmp1(31 downto 3);
-    result2 <= std_logic_vector(unsigned(B1)-unsigned("0010"&tmp1_2(31 downto 3)));
+    result2 <= unsigned(unsigned(B1)-unsigned("0010"&tmp1_2(31 downto 3)));
     
     --angle_reg_2 <= angle_reg_1;
-    tmp1_3 <= std_logic_vector(unsigned(angle_reg_2)*(unsigned("0000000000000"&z_reg_2(31 downto 13))));
+    tmp1_3 <= unsigned(unsigned(angle_reg_2)*(unsigned("0000000000000"&z_reg_2(31 downto 13))));
     result3 <= tmp1_3(31 downto 0);
     
     --angle_reg_3 <= angle_reg_2;
-    tmp1_4 <= std_logic_vector(unsigned(angle_reg_3)*(unsigned("0000000000000"&z_reg_3(31 downto 13))));
+    tmp1_4 <= unsigned(unsigned(angle_reg_3)*(unsigned("0000000000000"&z_reg_3(31 downto 13))));
     result4 <= tmp1_4(31 downto 0);
     
-    result5 <= std_logic_vector(unsigned(A1)-unsigned("0"&z_reg_4(31 downto 1)));
+    result5 <= unsigned(unsigned(A1)-unsigned("0"&z_reg_4(31 downto 1)));
     
-    tmp1_6 <= std_logic_vector(unsigned(angle_reg_5)*unsigned("0000000000000"&z_reg_5(31 downto 13)));
+    tmp1_6 <= unsigned(unsigned(angle_reg_5)*unsigned("0000000000000"&z_reg_5(31 downto 13)));
     result6 <= tmp1_6(31 downto 0);
     
-    tmp2_1 <= std_logic_vector(to_unsigned(1,32));
-    tmp2_2 <= std_logic_vector(unsigned(z_reg_6)+unsigned(tmp2_1(13 downto 0)&"000000000000000000"));
+    tmp2_1 <= unsigned(to_unsigned(1,32));
+    tmp2_2 <= unsigned(unsigned(z_reg_6)+unsigned(tmp2_1(13 downto 0)&"000000000000000000"));
     result7 <= "0000000000000000000"&tmp2_2(31 downto 19);
     
     result <= z_reg_7(15 downto 0);
